d4d3b87c68d2c186d9364a8923f4fcb0bc84f422
Brady Camp <bradyjcamp@gmail.com>
2022-02-16 19:03:43 -0500

    added modal when image clicked

--- src/App.js
+++ src/App.js
@@ -1,17 +1,66 @@
 import React from 'react';
 import './App.css';
-import Header from './Header.js'
-import Main from './Main.js'
-import Footer from './Footer.js'
+import Header from './Header.js';
+import SelectedBeast from './SelectedBeast.js';
+import Main from './Main.js';
+import Footer from './Footer.js';
+import data from './data.json';
 import 'bootstrap/dist/css/bootstrap.min.css';
+// import Modal from "react-bootstrap/Modal"
 
 
 class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      showModal: false,
+      selectedBeast: {}
+    }
+  }
+
+  handleCloseModal = () => {
+    this.setState({
+      showModal: false
+    });
+  }
+
+  
+  handleShowModal = (selectedBeast) => {
+    // const selectedBeast = data.find(beast => beast.title === name);
+    this.setState({
+      selectedBeast,
+      showModal: true,
+    });
+  }
+
+
   render(){
     return (
       <> 
       <Header />
-      <Main /> 
+      <SelectedBeast
+        handleCloseModal={this.handleCloseModal}
+        showModal={this.state.showModal}
+        selectedBeast={this.state.selectedBeast}
+
+      />
+
+
+      {/* <Modal show ={this.state.showModal} onHide={this.handleCloseModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>{this.state.name}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {this.state.name} is an important part of this community
+        </Modal.Body>
+
+
+      </Modal> */}
+
+
+      <Main 
+      data={data} 
+      handleShowModal={this.handleShowModal}/> 
       <Footer />
     </>
     );
--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -16,22 +16,28 @@ class HornedBeast extends React.Component{
     });
   }
 
+  handleAnimalClick = () => {
+    this.props.handleShowModal(this.props.selectedBeast);
+    this.handleFavorite();
+  }
+
   render(){
     return(
       <article>
         <Card 
         className="square border border-3 border-primary mx-auto mb-4" 
         style={{ width: '18rem' }}>
-          <Card.Img onClick={this.handleFavorite} 
-            src={this.props.imageUrl} 
-            alt={this.props.description} 
-            title={this.props.title}
+          <Card.Img 
+            src={this.props.selectedBeast.image_url} 
+            alt={this.props.selectedBeast.description} 
+            title={this.props.selectedBeast.title}
             variant="top" 
+            onClick={this.handleAnimalClick}
           />
           <Card.Body className="text-center">
-            <Card.Title>{this.props.title}</Card.Title>
+            <Card.Title>{this.props.selectedBeast.title}</Card.Title>
             <Card.Text>💖: {this.state.favorited}</Card.Text>
-            <Card.Text>{this.props.description}</Card.Text>
+            <Card.Text>{this.props.selectedBeast.description}</Card.Text>
           </Card.Body>
         </Card>
     </article>
